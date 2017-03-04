e090c8defbfb46fb4ce20dd7adfd003d4cbff027
niole <niolenelson@gmail.com>
2017-03-03 19:23:46 -0800

    removes aberrant object

--- BloomFilter.scala
+++ BloomFilter.scala
@@ -9,48 +9,50 @@ import MurmurHash3.ArrayHashing
  * how to size a bloom filter: minimize error rate given n, m, k values by adjusting them and then checking the error rate
  **/
 
-object Hasher extends Hashing[Array[Int]] {
-  def hash(number: Array[Int]): Int = number.hashCode
-}
+object BloomFilterSpec {
 
-class BloomFilter(maxData: Int, errorRate: Double, totalHashFunctions: Int = 2) {
-  private val hashFunctions: Seq[(Int) => Int] = Seq((x: Int) => new ByteswapHashing[Int]().hash(x), (x: Int) => new ArrayHashing[Int]().hash(intToArray(x)))
+  class BloomFilter(maxData: Int, errorRate: Double, totalHashFunctions: Int = 2) {
+    private val hashFunctions: Seq[(Int) => Int] = Seq((x: Int) => new ByteswapHashing[Int]().hash(x), (x: Int) => new ArrayHashing[Int]().hash(intToArray(x)))
 
-  private[this] var set = init
+    private[this] var set = init
 
-  private[this] def getErrorRate(k: Double, n: Double, m: Double): Double = Math.pow((1.0-Math.exp(-k*n/m)), k)
+    private[this] def getErrorRate(k: Double, n: Double, m: Double): Double = Math.pow((1.0-Math.exp(-k*n/m)), k)
 
-  private def intToArray(n: Int): Array[Int] = n.toString.map(_.toInt).toArray
+    private def intToArray(n: Int): Array[Int] = n.toString.map(_.toInt).toArray
 
-  private[this] def init: Array[Int] = {
-    var eRate = errorRate + 1
-    var size = maxData - 10
+    private[this] def init: Array[Int] = {
+      var eRate = errorRate + 1
+      var size = maxData - 10
 
-    while (eRate > errorRate) {
-     size += 10
-     eRate = getErrorRate(totalHashFunctions, maxData, size)
+      while (eRate > errorRate) {
+       size += 10
+       eRate = getErrorRate(totalHashFunctions, maxData, size)
+      }
+
+      Array.fill(size){ 0 }
+
     }
 
-    Array.fill(size){ 0 }
+    def getAll: Seq[Int] = set
+
+    def add(number: Int): Unit = {
+
+      hashFunctions.foreach(f => {
+        set(Math.abs(f(number)) % set.length) = 1
+      })
+
+    }
+
+    def contains(number: Int): Boolean = hashFunctions.forall(f => set(Math.abs(f(number)) % set.length) == 1)
 
   }
-
-  def getAll: Seq[Int] = set
-
-  def add(number: Int): Unit = {
-
-    hashFunctions.foreach(f => {
-      set(Math.abs(f(number)) % set.length) = 1
-    })
-
-  }
-
-  def contains(number: Int): Boolean = hashFunctions.forall(f => set(Math.abs(f(number)) % set.length) == 1)
 
 }
 
 
 object Main extends App {
+  import BloomFilterSpec._
+
   val bf = new BloomFilter(100, .05)
 
   bf.add(1234)
